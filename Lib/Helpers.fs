5882305331e35be2b9e6f4d9368d0d98b76574ee
jason-kerney <jason.kerney@edf-re.com>
2023-04-18 14:40:07 -0700

    feat: subfeature

--- Lib/Helpers.fs
+++ Lib/Helpers.fs
@@ -5,17 +5,22 @@ open System
 open System.Diagnostics
 open Archer.Arrows.Internal
 
+let private getNames () =
+    let trace = StackTrace ()
+    let method = trace.GetFrame(2).GetMethod ()
+    let containerName = method.ReflectedType.Name
+    let containerPath = method.ReflectedType.Namespace |> fun s -> s.Split ([|"$"|], StringSplitOptions.RemoveEmptyEntries) |> Array.last
+            
+    containerName, containerPath
+
 type Arrow =
     static member NewFeature () =
-        let featureName, featurePath =
-            let trace = StackTrace ()
-            let method = trace.GetFrame(1).GetMethod ()
-            let containerName = method.ReflectedType.Name
-            let containerPath = method.ReflectedType.Namespace |> fun s -> s.Split ([|"$"|], StringSplitOptions.RemoveEmptyEntries) |> Array.last
-                
-            containerName, containerPath
-            
+        let featureName, featurePath = getNames ()
         Feature (featurePath, featureName)
         
     static member NewFeature (featurePath, featureName) =
         Feature (featurePath, featureName)
+        
+    static member NewFeature (feature: Feature) =
+        let featureName, _ = getNames ()
+        feature.SubFeature featureName
