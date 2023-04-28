a0faa4fb228fa272b27c5a1740a5033c1b9eb9b1
Jason Kerney <robert.kerney@edf-re.com>
2023-04-28 10:31:52 -0700

    test: More Arrow tests

--- Test.Scripts/Arrow NewFeature With Setup.fs
+++ Test.Scripts/Arrow NewFeature With Setup.fs
@@ -27,6 +27,51 @@ let ``Should call setup when it is the only thing passed to it`` =
             |> Should.BeEqualTo true
             |> withMessage "Setup didn't run"
     )
+
+let ``Should call setup when name and setup are specified`` =
+    feature.Test (
+        fun _ ->
+            let monitor = Monitor (Ok ())
+                
+            let testFeature = Arrow.NewFeature (
+                "My magic feature",
+                Setup monitor.CallSetup
+            )
+            
+            let test = testFeature.Test (fun _ -> TestSuccess)
+            
+            test.GetExecutor ()
+            |> executeFunction
+            |> runIt
+            |> ignore
+            
+            monitor.SetupWasCalled
+            |> Should.BeEqualTo true
+            |> withMessage "Setup didn't run"
+    )
+
+let ``Should call setup when path, name, and setup are specified`` =
+    feature.Test (
+        fun _ ->
+            let monitor = Monitor (Ok ())
+                
+            let testFeature = Arrow.NewFeature (
+                "A Path",
+                "My magic feature",
+                Setup monitor.CallSetup
+            )
+            
+            let test = testFeature.Test (fun _ -> TestSuccess)
+            
+            test.GetExecutor ()
+            |> executeFunction
+            |> runIt
+            |> ignore
+            
+            monitor.SetupWasCalled
+            |> Should.BeEqualTo true
+            |> withMessage "Setup didn't run"
+    )
     
 let ``Should call setup when it is passed with a teardown`` =
     feature.Test (
@@ -34,6 +79,53 @@ let ``Should call setup when it is passed with a teardown`` =
             let monitor = Monitor (Ok ())
             
             let testFeature = Arrow.NewFeature (
+                Setup monitor.CallSetup,
+                Teardown (fun _ _ -> Ok ())
+            )
+            
+            let test = testFeature.Test (fun _ -> TestSuccess)
+            
+            test.GetExecutor ()
+            |> executeFunction
+            |> runIt
+            |> ignore
+            
+            monitor.SetupWasCalled
+            |> Should.BeEqualTo true
+            |> withMessage "Setup didn't run"
+    )
+    
+let ``Should call setup when name, setup, and teardown are specified`` =
+    feature.Test (
+        fun _ ->
+            let monitor = Monitor (Ok ())
+            
+            let testFeature = Arrow.NewFeature (
+                "Hello Feature",
+                Setup monitor.CallSetup,
+                Teardown (fun _ _ -> Ok ())
+            )
+            
+            let test = testFeature.Test (fun _ -> TestSuccess)
+            
+            test.GetExecutor ()
+            |> executeFunction
+            |> runIt
+            |> ignore
+            
+            monitor.SetupWasCalled
+            |> Should.BeEqualTo true
+            |> withMessage "Setup didn't run"
+    )
+    
+let ``Should call setup when path, name, setup, and teardown are specified`` =
+    feature.Test (
+        fun _ ->
+            let monitor = Monitor (Ok ())
+            
+            let testFeature = Arrow.NewFeature (
+                "Hello Path",
+                "Hello Feature",
                 Setup monitor.CallSetup,
                 Teardown (fun _ _ -> Ok ())
             )
--- Test.Scripts/Arrow NewFeature.fs
+++ Test.Scripts/Arrow NewFeature.fs
@@ -8,6 +8,19 @@ open Archer.Arrows.Tests
 
 let private feature = Arrow.NewFeature ()
 
+let private exampleWithSetup = Arrow.NewFeature (
+    Setup (fun _ -> Ok ())
+)
+
+let private exampleWithTeardown = Arrow.NewFeature (
+    Teardown (fun _ _ -> Ok ())
+)
+
+let private exampleWithBoth = Arrow.NewFeature (
+    Setup (fun _ -> Ok ()),
+    Teardown (fun _ _ -> Ok ())
+)
+
 type private Thing = {
     UnitProp: unit
 }
@@ -31,6 +44,30 @@ let ``Should Create a Feature with the name of the containing module`` =
             |> Should.BeEqualTo $"%s{containerPath}.%s{containerName}"
     )
     
+let ``Should Create a Feature with the name of the containing module even if setup added`` =
+    feature.Test (
+        fun _ ->
+            let containerPath, containerName = names
+            exampleWithSetup.ToString ()
+            |> Should.BeEqualTo $"%s{containerPath}.%s{containerName}"
+    )
+    
+let ``Should Create a Feature with the name of the containing module even if teardown added`` =
+    feature.Test (
+        fun _ ->
+            let containerPath, containerName = names
+            exampleWithTeardown.ToString ()
+            |> Should.BeEqualTo $"%s{containerPath}.%s{containerName}"
+    )
+    
+let ``Should Create a Feature with the name of the containing module even if both setup and teardown added`` =
+    feature.Test (
+        fun _ ->
+            let containerPath, containerName = names
+            exampleWithTeardown.ToString ()
+            |> Should.BeEqualTo $"%s{containerPath}.%s{containerName}"
+    )
+    
 let ``Should Create a Feature with the name and path given to it`` =
     feature.Test (
         fun _ ->
@@ -41,12 +78,104 @@ let ``Should Create a Feature with the name and path given to it`` =
             testFeature.ToString ()
             |> Should.BeEqualTo $"%s{path}.%s{name}"
     )
+    
+let ``Should Create a Feature with the name, path, and setup given to it`` =
+    feature.Test (
+        fun _ ->
+            let path = "This feature's path"
+            let name = "This feature's name"
+            let testFeature = Arrow.NewFeature (
+                path,
+                name,
+                Setup (fun _ -> Ok ())
+            )
+            
+            testFeature.ToString ()
+            |> Should.BeEqualTo $"%s{path}.%s{name}"
+    )
+    
+let ``Should Create a Feature with the name, path, and teardown given to it`` =
+    feature.Test (
+        fun _ ->
+            let path = "This feature's path"
+            let name = "This feature's name"
+            let testFeature = Arrow.NewFeature (
+                path,
+                name,
+                Teardown (fun _ _ -> Ok ())
+            )
+            
+            testFeature.ToString ()
+            |> Should.BeEqualTo $"%s{path}.%s{name}"
+    )
+    
+let ``Should Create a Feature with the name, path, setup and teardown given to it`` =
+    feature.Test (
+        fun _ ->
+            let path = "This feature's path"
+            let name = "This feature's name"
+            let testFeature = Arrow.NewFeature (
+                path,
+                name,
+                Setup (fun _ -> Ok ()),
+                Teardown (fun _ _ -> Ok ())
+            )
+            
+            testFeature.ToString ()
+            |> Should.BeEqualTo $"%s{path}.%s{name}"
+    )
 
 let ``Should Create a feature with the name given to it`` =
     feature.Test (
         fun _ ->
             let name = "A specified name"
             let testFeature = Arrow.NewFeature name
+            
+            testFeature.ToString ()
+            |> Should.PassTestOf (fun featureName ->
+                featureName.EndsWith $".%s{name}"
+            )
+    )
+
+let ``Should Create a feature with the name and setup given to it`` =
+    feature.Test (
+        fun _ ->
+            let name = "A specified name"
+            let testFeature = Arrow.NewFeature (
+                name,
+                Setup (fun _ -> Ok ())
+            )
+            
+            testFeature.ToString ()
+            |> Should.PassTestOf (fun featureName ->
+                featureName.EndsWith $".%s{name}"
+            )
+    )
+
+let ``Should Create a feature with the name and teardown given to it`` =
+    feature.Test (
+        fun _ ->
+            let name = "A specified name"
+            let testFeature = Arrow.NewFeature (
+                name,
+                Teardown (fun _ _ -> Ok ())
+            )
+            
+            testFeature.ToString ()
+            |> Should.PassTestOf (fun featureName ->
+                featureName.EndsWith $".%s{name}"
+            )
+    )
+
+let ``Should Create a feature with the name, setup and teardown given to it`` =
+    feature.Test (
+        fun _ ->
+            let name = "A specified name"
+            let testFeature = Arrow.NewFeature (
+                name,
+                Setup (fun _ -> Ok ()),
+                Teardown (fun _ _ -> Ok ())
+            )
             
             testFeature.ToString ()
             |> Should.PassTestOf (fun featureName ->
