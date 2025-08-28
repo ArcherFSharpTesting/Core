a4995d8e97fc3ee58fcf46b2671a4bd440170fe4
Jason Kerney <Jason@TeamKerney.com>
2025-08-28 09:11:55 -0700

    refactor: breaking up big files

---
+++ Lib/Arrow.Tags.NoParam.fs
@@ -0,0 +1,16 @@
+[<AutoOpen>]
+module Archer.Arrows.ArrowTagsNoParam
+
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+open Archer.Arrows.Helpers
+
+type Arrow with
+
+    static member NewFeature (featureTags: TagsIndicator) =
+        let featureName, featurePath = getNames ()
+        Arrow.NewFeature (featurePath, featureName, featureTags)
+
+    static member Ignore (featureTags: TagsIndicator, [<CallerFilePath; Optional; DefaultParameterValue("")>] fileFullName: string, [<CallerLineNumber; Optional; DefaultParameterValue(-1)>] lineNumber: int) =
+        let featureName, featurePath = getNames ()
+        Arrow.Ignore (featurePath, featureName, featureTags, fileFullName, lineNumber)
