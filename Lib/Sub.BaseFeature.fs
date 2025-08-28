82968c977ae6a0ffcba5795b44c5f38401406d50
Jason Kerney <Jason@TeamKerney.com>
2025-08-28 10:35:00 -0700

    docs: doc comments

--- Lib/Sub.BaseFeature.fs
+++ Lib/Sub.BaseFeature.fs
@@ -8,7 +8,18 @@ open Archer.Arrows.Internal.Types
 open Archer.Arrows.Internals
 open Archer.CoreTypes.InternalTypes
 
+/// <summary>
+/// Provides methods for creating sub-features within a test feature.
+/// </summary>
 type Sub =
+    /// <summary>
+    /// Creates a sub-feature for a given feature, allowing custom setup and teardown logic.
+    /// </summary>
+    /// <param name="subFeatureName">The name of the sub-feature.</param>
+    /// <param name="featureTags">Tags to associate with the sub-feature.</param>
+    /// <param name="setup">Setup logic for the sub-feature.</param>
+    /// <param name="teardown">Teardown logic for the sub-feature.</param>
+    /// <returns>A function that takes a parent feature and returns the constructed sub-feature.</returns>
     static member Feature (subFeatureName, TestTags featureTags, setup: SetupIndicator<'featureType, 'subFeatureType>, teardown: TeardownIndicator<'subFeatureType>) =
         let buildIt (feature: IFeature<'featureType>) =
             let builder =
@@ -28,6 +39,16 @@ type Sub =
 
         buildIt
 
+    /// <summary>
+    /// Creates an ignored sub-feature for a given feature, allowing custom setup and teardown logic.
+    /// </summary>
+    /// <param name="subFeatureName">The name of the sub-feature.</param>
+    /// <param name="featureTags">Tags to associate with the sub-feature.</param>
+    /// <param name="setup">Setup logic for the sub-feature.</param>
+    /// <param name="teardown">Teardown logic for the sub-feature.</param>
+    /// <param name="fileFullName">The full file path where the ignore is declared (automatically provided).</param>
+    /// <param name="lineNumber">The line number where the ignore is declared (automatically provided).</param>
+    /// <returns>A function that takes a parent feature and returns the constructed ignored sub-feature.</returns>
     static member Ignore (subFeatureName, TestTags featureTags, setup: SetupIndicator<'featureType, 'subFeatureType>, teardown: TeardownIndicator<'subFeatureType>, [<CallerFilePath; Optional; DefaultParameterValue("")>] fileFullName: string, [<CallerLineNumber; Optional; DefaultParameterValue(-1)>] lineNumber: int) =
         let buildIt (feature: IFeature<'featureType>) =
             let builder =
